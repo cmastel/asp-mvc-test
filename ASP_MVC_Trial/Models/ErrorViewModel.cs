38b4313d0704351fb3f4987d48d97cda1e23aad2
Chris <chris.mastel@gmail.com>
2020-06-21 07:22:07 -0600

    initial commit

---
+++ ASP_MVC_Trial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_MVC_Trial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
